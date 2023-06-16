266b66d2dab330af30152ad2c4b0aeb5cb8a7449
jofercho <ssoj.josss@gmail.com>
2023-06-16 12:27:10 -0500

    first commit

---
+++ demoDj/src/main/java/com/jose/tes/demoDj/DemoDjApplication.java
@@ -0,0 +1,13 @@
+package com.jose.tes.demoDj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDjApplication.class, args);
+	}
+
+}
---
+++ demoDj/src/test/java/com/jose/tes/demoDj/DemoDjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.tes.demoDj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
